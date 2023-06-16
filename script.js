bb9fe473757f330b73329bcd96752c123c147471
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-06-16 16:29:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow", 
+    grabCursor: true, 
+    centeredSlides: true, 
+    slidesPerView:"auto", 
+    coverFlowEffect:{
+        rotate:15, 
+        strech:0,
+        depth:300, 
+        modifier:1, 
+        slideShadows: true
+    },
+    loop:true
+})
